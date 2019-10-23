642b375d6f0223c977fd2e650ae15ffaec8aab8d
Korusuke <karan.sheth@somaiya.edu>
2019-10-23 21:30:51 +0530

    Typos

--- js/input.js
+++ js/input.js
@@ -1,7 +1,7 @@
 $(function() {
   // Set the command-line prompt to include the user's IP Address
   $('.prompt').html('[' + "127.0.0.1" + '@HTML5] # ');
-  $(".prompt").html("<b style=\"font-family:monospace\">KJSCE@codecell-macbook-pro</b>$");
+  $(".prompt").html("<b style=\"font-family:monospace\">KJSCE@CodeCell-macbook-pro</b>$");
   $('.prompt').html('>>');
 
   // Initialize a new terminal object
