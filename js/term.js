105f0db73b34baf3cbc47781e434cd0f04099d60
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2021-09-19 16:41:49 +0530

    fix: variable

--- js/term.js
+++ js/term.js
@@ -188,7 +188,7 @@ var Terminal =
                         output("SUBMITTING DETAILS... PLEASE WAIT");
                         try {
                             var response = await fetch(
-                                `https://docs.google.com/forms/d/e/1FAIpQLSd8GSiLY0sXxryyrIPDvjjJiodeH6-TLcChEBVB0-4AtyT0mA/viewform?usp=pp_url&entry.1724760141=${ans.Email}&entry.580448289=${ans.Name}&entry.1284286654=${ans.mobile}&entry.405986999=${ans.q1_skill_level}&entry.405986999.other_option_response=${ans.college}&entry.1414498390=${ans.state}&entry.158599955=${ans.city}&entry.2006066848=${ans.year}&entry.1536465481=__other_option__&entry.1536465481.other_option_response=${ans.branch}&entry.553706898=1&entry.792650617=${ans.q2_track}&entry.1056831604=${q3_expectations}&entry.1145970256=${q4_comments}`,
+                                `https://docs.google.com/forms/d/e/1FAIpQLSd8GSiLY0sXxryyrIPDvjjJiodeH6-TLcChEBVB0-4AtyT0mA/viewform?usp=pp_url&entry.1724760141=${ans.Email}&entry.580448289=${ans.Name}&entry.1284286654=${ans.mobile}&entry.405986999=${ans.q1_skill_level}&entry.405986999.other_option_response=${ans.college}&entry.1414498390=${ans.state}&entry.158599955=${ans.city}&entry.2006066848=${ans.year}&entry.1536465481=__other_option__&entry.1536465481.other_option_response=${ans.branch}&entry.553706898=1&entry.792650617=${ans.q2_track}&entry.1056831604=${ans.q3_expectations}&entry.1145970256=${q4_comments}`,
                                 {
                                     method: "POST", mode: "cors", cache: "no-cache", credentials: "same-origin",
                                     headers: {
